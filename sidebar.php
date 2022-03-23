df2ebb5b9c35a9c3994e7582d08edb41e9dc54c5
Sadness <95544008+sadgrlonline@users.noreply.github.com>
2022-03-23 14:30:49 -0400

    added sidebar

---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<div class="sideBox">
+    <strong>Visit our site @</strong><br>
+    <a href="https://yesterweb.org/" target="_blank">Yesterweb.org</a>
+</div>
+
+<div class="sideBox webring">
+    <strong>Yesterweb Ring Member</strong>
+    <br><em>of the moment:</em><br>
+</div>
