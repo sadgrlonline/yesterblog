e9cebc9669d9e761d1a114bc364b8fb8592160c6
Sadness <95544008+sadgrlonline@users.noreply.github.com>
2022-04-01 09:15:38 -0400

    minor updates in random entry script

--- random.php
+++ random.php
@@ -14,8 +14,9 @@ while ($row = $result->fetch_assoc()) {
     $title = $row['title'];
     $entry = $row['entry'];
     echo "<div class='feature'>";
-    echo "<a href='view.php?entry=" . $id .  "'>" . $title . "</a><br>";
-    echo "<p>" . strip_tags(substr($entry, 0, 250)) . "... <a href='view.php?entry=" . $id .  "'>Read more...</a></p>";
+    echo "<div class='smTitle'><a href='view.php?entry=" . $id .  "'>" . $title . "</a></div>";
+    echo "<p>by <em>" . $name . "</em></p>";
+    echo "<p>" . strip_tags(substr($entry, 0, 250)) . "... <a class='readMore'  href='view.php?entry=" . $id .  "'>Read more...</a></p>";
     echo "</div>";
 }
 $stmt->close();
